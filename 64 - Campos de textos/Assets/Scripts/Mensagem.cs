232717229c1699389a419400f835256a9db6dc8f
marcosporto <marcosportoudesc@gmail.com>
2018-12-08 23:35:29 -0200

    Uso do Toglle ou caixa de seleção.

---
+++ 64 - Campos de textos/Assets/Scripts/Mensagem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Importação de nameSpace
+using UnityEngine.UI;
+
+public class Mensagem : MonoBehaviour {
+
+    public Text txt;
+    public InputField inputName; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MostraMesagem() {
+
+        txt.text = "Seja bem vindo(a): " + inputName.text;
+    }
+}
