0068ef95cf11f2457ea4c6b815c86996767d5ce6
marcosporto <marcosportoudesc@gmail.com>
2018-12-10 18:02:16 -0200

    Uso de Slider e conexão com som.

---
+++ 67 - Slider (Controle deslizante)/Assets/Scripts/Volume.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Audio;
+
+public class Volume : MonoBehaviour {
+
+    public AudioMixer controleSom;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetVolume (float volume) {
+
+        controleSom.SetFloat("volume", volume);
+    }
+}
