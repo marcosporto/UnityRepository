765ad3e8989bd6620aa97870d1e350ea8f351f77
marcosporto <marcosportoudesc@gmail.com>
2018-12-11 19:30:00 -0200

    Mudança de cenas.

---
+++ 69 - Aplicação prática/Assets/Scripts/ControleMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement; // Usado para controlar às cenas
+
+public class ControleMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    // Método para chamar uma nova cena
+    public void ChamaCena( string nome) {
+
+        SceneManager.LoadScene(nome);
+
+    }
+}
