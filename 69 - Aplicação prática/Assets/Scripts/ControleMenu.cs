c11d05410b8f33f0b238fd94be52c876ced879d0
marcosporto <marcosportoudesc@gmail.com>
2018-12-12 15:24:54 -0200

    Finalização da inserção e teste do som.

--- 69 - Aplicação prática/Assets/Scripts/ControleMenu.cs
+++ 69 - Aplicação prática/Assets/Scripts/ControleMenu.cs
@@ -8,35 +8,23 @@ using UnityEngine.SceneManagement; // Usado para controlar às cenas
 
 public class ControleMenu : MonoBehaviour {
 
-    public AudioMixer controleSom;
-
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-
+    public AudioMixer AudioMixerSom;
+    
     // Método para chamar uma nova cena
     public void ChamaCena( string nome) {
-
         SceneManager.LoadScene(nome);
-
     }
 
     // Sai da aplicação
     public void sair() {
         Application.Quit();
     }
-    // Controle do áudio
+
+    // Método para controle de áudio
+    // No movimento do Slider é passado o volume que é do tipo float
     public void SetVolume( float volume) {
-        // Seta 
-        controleSom.SetFloat("Volume", volume);
+        // No AudioMixer é setado o valor recebido pelo Slider
+        AudioMixerSom.SetFloat("Volume", volume);
         Debug.Log("Volume: " + volume);
     }
 }
