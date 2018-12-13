44c2094df7e6e406be270623306bc1509ad0a206
marcosporto <marcosportoudesc@gmail.com>
2018-12-13 01:12:02 -0200

    Cronômetro e contador finalizados.

---
+++ 72 - Contador regressivo/Assets/Scripts/ContadorRegressivo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ContadorRegressivo : MonoBehaviour {
+
+    public float _tempoInicial = 60;
+    public Text _contadorRegressivo;
+
+	// Use this for initialization
+	void Start () {
+        _contadorRegressivo.text = _tempoInicial.ToString();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if(_tempoInicial >= 0) {
+
+            _tempoInicial -= Time.deltaTime; // Normaliza os segundos -> Time.deltaTime
+            _contadorRegressivo.text = Mathf.Round(_tempoInicial).ToString(); // Arredonda os valores em float -> Mathf.Round();
+        }
+    }
+}
---
+++ 72 - Contador regressivo/Assets/Scripts/Cronometro.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Cronometro : MonoBehaviour {
+
+    public float _tempoInicial = 0;
+    public Text _cronometro;
+    public Text _textoBotao;
+
+    bool _cronometroAtivo = false;
+
+	// Use this for initialization
+	void Start () {
+
+        _cronometro.text = _tempoInicial.ToString("F2") + "m"; // Formatação para 2 campos
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (_cronometroAtivo) {
+
+            _tempoInicial += Time.deltaTime;
+            _cronometro.text = _tempoInicial.ToString("F2") + "m";
+        }		
+	}
+    public void BotaoTempo() {
+
+        _cronometroAtivo = !_cronometroAtivo;
+        // Recurso do C#: cria uma condição, se for verdadeira armazena o primeiro texto, e se for falsa, armazena o segundo
+        _textoBotao.text = _cronometroAtivo ? "Pausar" : "Iniciar";
+    }
+}
