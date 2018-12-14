1d9fcb2868293aafa9959c3ab7e11fe15ae2e1df
marcosporto <marcosportoudesc@gmail.com>
2018-12-14 01:01:13 -0200

    Botões do mouse finalizado.

---
+++ 74 - Imput teclado/Assets/Scripts/Teclado.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teclado : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Aciona quando a tecla é pressionada
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Tecla espaço foi pressionada!");
+        }
+        // Aciona quando a tecla é solta
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            Debug.Log("Tecla espaço foi liberada!");
+        }
+    }
+}
---
+++ 75 - Botões do mouse/Assets/Scripts/Mouse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour {
+
+    public Texture2D mousePonteiro;
+
+	// Use this for initialization
+	void Start () {
+        // Função executa à troca do ponteiro do mouse
+        Cursor.SetCursor(mousePonteiro, Vector2.zero, CursorMode.Auto);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // 0 esquerda
+        // 1 direita
+        // 2 meio
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("Botão do mouse esquedo foi pressionado");
+        }
+        if (Input.GetMouseButtonDown(1)) {
+            Debug.Log("Botão do mouse direito foi pressionado");
+        }
+        if (Input.GetMouseButtonDown(2)) {
+            Debug.Log("Botão do mouse do meio foi pressionado");
+        }
+    }
+}
