99fa49bb710f73d0a411bf1b2639a2afab43f85d
marcosporto <marcosportoudesc@gmail.com>
2018-12-16 16:27:03 -0200

    ...

---
+++ Controle IDLE e RUN/Assets/Scripts/ControllerPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private Animator    playerAnimator;
+    private Rigidbody2D playerRigidBody2D;
+    public Transform    groundCheck;
+
+    public bool isGrounded;
+    public bool facingRight = true;
+
+    public float speed;
+    public float direcao;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
