f73f04a5df049a3bcf69dd568f52506de26e0692
marcosporto <marcosportoudesc@gmail.com>
2018-12-23 06:20:42 -0200

    ...

--- Crossy Road 3D/Assets/Scripts/Manager.cs
+++ Crossy Road 3D/Assets/Scripts/Manager.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum GameState {
+
+    TITULO,
+    GAMEMPLAY,
+    GAMEOVER,
+    FASECONCLUIDA
+}
+
+
 public class Manager : MonoBehaviour {
+
+    public GameState currentState;
 
     public GameObject hudInfoGamePlay, hudTitulo, hudGameOver, hudLoading, hudLoja;
     public Text moedasTxt, faseTxt, moedasMapaTxt, tomatesMapaTxt, tempoTxt;
+    public int moedas, fase, moedasMapa, tomatesMapa, tempo, moedasColetadas, tomatesColetados;
 
 
     // Use this for initialization
     void Start () {
-		
-	}
+
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
