6a08b66f0cea62d942b0f95123b4bde257f3eaed
marcosporto <marcosportoudesc@gmail.com>
2018-12-27 01:45:17 -0200

    Navegação dos Botões pelo Teclado

---
+++ Crossy Road 3D/Assets/Scripts/btnAtivo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;   // Name space usado para trabalhar com botões
+
+public class btnAtivo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        // Captura o botão vinculado e seleciona o mesmo
+        GetComponent<Button>().Select();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
