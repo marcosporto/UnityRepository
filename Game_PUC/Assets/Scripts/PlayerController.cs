f341b59b7080e38eb9c8ab6f35ab215b52b611cb
marcosporto <marcosportoudesc@gmail.com>
2020-02-06 16:34:47 -0300

    i

---
+++ Game_PUC/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidadeDoPlayer = 20.0f;
+    private Vector3 posicaoDoPlayer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posicaoDoPlayer = transform.position;
+        posicaoDoPlayer.x += velocidadeDoPlayer * Input.GetAxis("Horizontal") * Time.deltaTime;
+        posicaoDoPlayer.y += velocidadeDoPlayer * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position = posicaoDoPlayer;
+    }
+}
