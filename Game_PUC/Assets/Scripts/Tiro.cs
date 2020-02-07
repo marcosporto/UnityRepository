5b62ed181ec45635f2c67c7cb67dc21e37388ce2
marcosporto <marcosportoudesc@gmail.com>
2020-02-07 17:27:12 -0300

    Criação do tiro da arma, porém está subindo.

---
+++ Game_PUC/Assets/Scripts/Tiro.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiro : MonoBehaviour
+{
+    public GameObject municaoPrefab;
+    public Transform posicaoDaArma;
+    public float velocidadeMunicao;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            if (!IsInvoking("InstantiateMunicao")) {
+
+                InvokeRepeating("InstantiateMunicao", 0f, 1f);
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) {
+
+            CancelInvoke("InstantiateMunicao");
+        }
+    }
+
+    void InstantiateMunicao() {
+
+        if(municaoPrefab != null) {
+
+            GameObject municaoTemporaria = Instantiate(municaoPrefab) as GameObject;
+            municaoTemporaria.transform.position = posicaoDaArma.position;
+            municaoTemporaria.GetComponent<Rigidbody>().velocity = transform.parent.forward * velocidadeMunicao;
+        }
+    }
+}
