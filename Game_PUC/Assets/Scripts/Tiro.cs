3cb1b73af87ad9a9bc8e9b485c2cb35f06ad15d4
marcosporto <marcosportoudesc@gmail.com>
2020-02-08 18:02:43 -0300

    .

--- Game_PUC/Assets/Scripts/Tiro.cs
+++ Game_PUC/Assets/Scripts/Tiro.cs
@@ -11,14 +11,17 @@ public class Tiro : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        Touch touch = Input.GetTouch(0);
+
+        if (Input.GetKeyDown(KeyCode.Space) || touch.phase == 0)
+        {
 
             if (!IsInvoking("InstantiateMunicao")) {
 
                 InvokeRepeating("InstantiateMunicao", 0f, 1f);
             }
         }
-        if (Input.GetKeyUp(KeyCode.Space)) {
+        if (Input.GetKeyUp(KeyCode.Space) || touch.phase == 0) {
 
             CancelInvoke("InstantiateMunicao");
         }
