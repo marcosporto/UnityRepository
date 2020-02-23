e1087f41b4ddba3c955a6ed481f206f57c73b30b
marcosporto <marcosportoudesc@gmail.com>
2020-02-23 02:10:44 -0300

    Mira já está se movimentando na horizontal.

--- Game_PUC/Assets/Scripts/Destruir.cs
+++ Game_PUC/Assets/Scripts/Destruir.cs
@@ -4,11 +4,60 @@ using UnityEngine;
 
 public class Destruir : MonoBehaviour
 {
+    public Touch touch;
+    public float velocidadeDaMira;
+
+    private void Start()
+    {
+        velocidadeDaMira = 0.5f;
+    }
+
+    private void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                transform.position = new Vector3(
+                    transform.position.x + touch.deltaPosition.x * velocidadeDaMira,
+                    transform.position.y,
+                    transform.position.z);
+            }
+        }
+
+       // for (int i = 0; i < Input.touchCount; i++)
+       // {
+         //   print("A posição dos dedos é " + Input.touches[i].position);
+       // }
+
+
+
+       // Touch touch = Input.GetTouch(0);
+       //Vector3 touchPosition = Input.GetTouch(0).deltaPosition;
+        //Camera.main.ScreenToWorldPoint(touch.position);
+        //touchPosition.y = -7.5f;
+
+       // transform.position = touchPosition;
+        //Debug.Log("Clique na posição: " + touchPosition)//
+    }
+            
+        //Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Stationary)
+        //{
+         //   Vector2 posicaoToqueNaTela = Input.GetTouch(0).deltaPosition;
+            
+         //   Debug.Log("Clique na posição: " + posicaoToqueNaTela.y);
+
+       // }
+    
+    public AudioSource audioDeTiro;
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Zumbi")
         {
             Debug.Log("Clicou no botão de TIRO!!!");
+            audioDeTiro.Play();
             Destroy(other.gameObject);
         }
     }
